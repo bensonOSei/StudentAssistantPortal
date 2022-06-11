6c6df9f45ed089e21ecc1eb2698df854ab52c888
mk-armah <mikeyarmah@gmail.com>
2022-06-11 13:21:18 +0000

    First Commit (Email Configured and HTML Mounted)

---
+++ notification.py
@@ -0,0 +1,36 @@
+from typing import List
+from fastapi_mail.email_utils import DefaultChecker
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from dotenv import dotenv_values
+from pydantic import BaseModel,Field,EmailStr
+
+credentials = dotenv_values(".env")
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+
+class NotifyUser:
+    def __init__(self,subject:str, message:str, email:list[str],credentials:dict = credentials):
+        
+        self.credentials = credentials
+
+        self.config = ConnectionConfig(
+            MAIL_USERNAME = self.credentials["EMAIL"],
+            MAIL_PASSWORD = self.credentials["PASSWORD"],
+            MAIL_FROM = self.credentials["EMAIL"],
+            MAIL_PORT = 587,
+            MAIL_SERVER = "smtp.gmail.com",
+            MAIL_FROM_NAME="E-LEARNING UNIT COLLAGE OF DISTANCE EDU",
+            MAIL_TLS = True,
+            MAIL_SSL = False,
+            USE_CREDENTIALS = True)
+         
+        self.message = MessageSchema(
+            subject= subject, # subject of the email
+            attachments= ["./requirements.txt"],
+            recipients = email, # List of recipients, as many as you can pass 
+            body= message, # message to be sent
+            subtype="html"
+            )
