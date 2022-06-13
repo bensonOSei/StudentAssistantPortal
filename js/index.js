a4930db85ae962bc4f0db26239ecb4048ef31bd3
bensonOSei <kbenson643@gmail.com>
2022-06-13 17:31:57 +0000

    UI Template Update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $("#openComp").click(() => {
+    $("#complaint").toggle("slow", () => {
+      $("#openComp").toggleClass("active");
+      $("#openAccess").toggleClass("active", () => {
+        $("#openComp").css("background-color", "red");
+      });
 
-    $("#openComp").click(() =>{
-        $("#compliant").toggle('slow', ()=>{
-            $("#openComp").toggleClass("active");
-            $("#openAccess").toggleClass("active",()=>{
-                $("#openComp").css("background-color","red")
-            });
-
-            $("#access_module").toggle()
-        });
-
+      $("#access_module").toggle();
     });
 
-    $("#openAccess").click(() =>{
-        $("#access_module").toggle('slow', ()=>{
-            $("#openAccess").toggleClass("active");
-            $("#openComp").toggleClass("active");
-
-            $("#compliant").toggle()
-        });
-
-        
-    });
-  
   });
+
+  $("#openAccess").click(() => {
+    $("#access_module").toggle("slow", () => {
+      $("#openAccess").toggleClass("active");
+      $("#openComp").toggleClass("active");
+
+      $("#complaint").toggle();
+    });
+
+  });
+});
