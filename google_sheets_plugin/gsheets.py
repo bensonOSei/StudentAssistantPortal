ed5e6198bf64d8843a01501dcd2a1d379b0ced0f
mk-armah <mikeyarmah@gmail.com>
2022-07-10 19:16:55 +0000

    Refactoring Codebase

--- google_sheets_plugin/gsheets.py
+++ google_sheets_plugin/gsheets.py
@@ -14,7 +14,11 @@ credentials = dotenv_values(".env")
 class GoogleSheets:
     """Google Sheets Class"""
 
-    def __init__(self, credentials_file: str, sheet_key: str, worksheet_name: str):
+    def __init__(
+            self,
+            credentials_file: str,
+            sheet_key: str,
+            worksheet_name: str):
         self.credentials_file = credentials_file
         self.sheet_key = sheet_key
         self.worksheet_name = worksheet_name
@@ -23,7 +27,7 @@ class GoogleSheets:
         ]
         self.sheet_object = self._get_sheet_object()
 
-    def _get_sheet_object(self): #-> gspread.models.Worksheet:
+    def _get_sheet_object(self):  # -> gspread.models.Worksheet:
         """get google sheet object"""
 
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(
@@ -32,7 +36,9 @@ class GoogleSheets:
 
         client = gspread.authorize(credentials)
 
-        return client.open_by_key(self.sheet_key).worksheet(self.worksheet_name)
+        return client.open_by_key(
+            self.sheet_key).worksheet(
+            self.worksheet_name)
 
     def write_header_if_doesnt_exist(self, columns: List[str]) -> None:
         """write the columns for the google sheet if there is None """
@@ -47,12 +53,22 @@ class GoogleSheets:
         last_row_number = len(self.sheet_object.col_values(1)) + 1
         self.sheet_object.insert_rows(rows, last_row_number)
 
-    
-if __name__== "__main__":
-    google_sheets = GoogleSheets(
-                                credentials_file= "gsheets_keys.json",
-                                sheet_key = credentials["GOOGLE_SHEET_ID"],
-                                worksheet_name = credentials['GOOGLE_SHEET_NAME'])
 
-    google_sheets.write_header_if_doesnt_exist(["Program","Course","Study_Center","Name","Registration_ID","Email","Complaint"])
-    google_sheets.append_rows([["Education","Early Childhood","Cape Coast","Michael Kofi Armah","BECE/CR/01/17/0001","mikeyarmah@gmail.com","modules"]])
+if __name__ == "__main__":
+    GOOGLE_CREDENTIALS = os.environ.get("GOOGLE_CREDENTIALS")
+    GOOGLE_CREDENTIALS = dict(json.loads(GOOGLE_CREDENTIALS))
+
+    google_sheets = GoogleSheets(
+        credentials_file=GOOGLE_CREDENTIALS,
+        sheet_key=credentials["GOOGLE_SHEET_ID"],
+        worksheet_name=credentials['GOOGLE_SHEET_NAME'])
+
+    google_sheets.write_header_if_doesnt_exist(
+        ["Program", "Course", "Study_Center", "Name", "Registration_ID", "Email", "Complaint"])
+    google_sheets.append_rows([["Electrical Engineering",
+                                "Power Electronics",
+                                "Accra",
+                                "Michael Kofi Armah",
+                                "123456789",
+                                "user@example.com",
+                                "i need my modules"]])
